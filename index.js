a3037d4382adb29e83a226b6bbce6b9cfc647bdd
RajiAujla <rajwinderaujla061@gmail.com>
2024-07-15 22:36:08 -0400

    Adding an item to db

--- index.js
+++ index.js
@@ -19,11 +19,13 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// function to fetch list from database
 async function fetchItems(){
   const result = await db.query("SELECT * FROM items");
   return result.rows;
 }
 
+//fetch all the items of list
 app.get("/", async (req, res) => {
   const toDoList = await fetchItems();
   console.log(toDoList)
@@ -33,9 +35,23 @@ app.get("/", async (req, res) => {
   });
 });
 
-app.post("/add", (req, res) => {
+// addiing a new item to list
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+    try{
+       const itemFound = await db.query("SELECT id FROM items where Lower(title) = $1",[item.toLowerCase()]);
+       try{
+        if(itemFound.rowCount == 0){
+          await db.query("INSERT Into Items(title) values ($1)",[item]);
+        }
+       }catch(error){
+        console.log("Insert error");
+        console.log(error);
+       }
+    }catch(error){
+      console.log("Itm not found")
+      console.log(error);
+    }
   res.redirect("/");
 });
 
